50415d581bb6e5aef3f72a4483a6d97e2155d578
Abdoulaye Dia <abdoulayedia@macbookpro-1.home>
2025-05-28 19:55:35 +0200

    📐 Implemented most of the domain logic for reservation and spots

--- backend/PRS.Domain/Entities/User.cs
+++ backend/PRS.Domain/Entities/User.cs
@@ -1,9 +1,45 @@
+using PRS.Domain.Core;
+using PRS.Domain.Errors;
+
 namespace PRS.Domain.Entities;
 
-public class User
+public partial class User
 {
-    public Guid Id { get; init; } = Guid.NewGuid();
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public required UserRole Role { get; set; }
+    public Guid Id { get; init; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public UserRole Role { get; private set; }
+
+    private User(Guid id, string name, string email, UserRole role)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Role = role;
+    }
+
+
+    public static Result<User> Create(string name, string email, UserRole role)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return Result<User>.Failure(new DomainError(
+                        "User.InvalidName", "Name required", "User name cannot be empty."));
+        }
+
+        if (string.IsNullOrWhiteSpace(email))
+        {
+            return Result<User>.Failure(new DomainError(
+                        "User.InvalidEmail", "Email required", "User email cannot be empty."));
+        }
+
+        if (role is null)
+        {
+            return Result<User>.Failure(new DomainError(
+                        "User.MissingRole", "Role required", "User must have a role."));
+        }
+
+        return Result<User>.Success(new User(Guid.NewGuid(), name, email, role));
+    }
+
 }
