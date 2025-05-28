d5d8f219157be16b9e224bddacb92bd7818c835c
Valentin Barbotin <valentinbarbotin@pm.me>
2025-05-28 16:52:34 +0200

    chore: clean

--- frontend/libs/shared-angular/src/index.ts
+++ frontend/libs/shared-angular/src/index.ts
@@ -4,5 +4,4 @@ export * from './lib/input-date/input-date.component';
 export * from './lib/input-password/input-password.component';
 export * from './lib/input-textarea/input-textarea.component';
 export * from './lib/input-select/input-select.component';
-export * from './lib/file-upload.component';
 export * from './lib/images-viewer/images-viewer.component';
