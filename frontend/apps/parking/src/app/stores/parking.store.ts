103811327efc540c6c8f315026ae65c2510442cb
Valentin Barbotin <valentinbarbotin@pm.me>
2025-05-28 17:09:19 +0200

    parking slot

---
+++ frontend/apps/parking/src/app/stores/parking.store.ts
@@ -0,0 +1,20 @@
+import { signalStore, withState, withMethods, withComputed, patchState } from '@ngrx/signals';
+import { debounceTime, distinct, exhaustMap, pipe, repeat, switchMap, tap } from 'rxjs';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { tapResponse } from '@ngrx/operators';
+import { computed, inject, linkedSignal } from '@angular/core';
+import { ParkingService } from '../services/parking.service';
+import { Router } from '@angular/router';
+
+export interface ParkingState {
+}
+
+export const ParkingStore = signalStore(
+    withState<ParkingState>({
+    }),
+    withComputed((store) => {
+    }),
+    withMethods((store, service = inject(ParkingService), router = inject(Router)) => ({
+        
+    }))
+);
